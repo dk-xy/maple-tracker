32064f7f0a8c56c319cebf3ba475c9ac428272e9
dk-xy <92148260+dk-xy@users.noreply.github.com>
2024-05-08 12:00:48 +0200

    better menu

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,8 @@
 // Layout.jsx (your common layout component)
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { render } from 'react-dom'
+import { StatsChart, People, Checkbox } from 'react-ionicons'
 
 
 const Layout = ({ children }) => {
@@ -9,12 +11,30 @@ const Layout = ({ children }) => {
             <nav>
                 <ul className='appMenu'>
                     <li className='menuElement'>
+                        <StatsChart className='icon'
+                            color={'#00000'}
+                            title={'Dashboard'}
+                            height="25px"
+                            width="25px"
+                        />
                         <Link to="/dashboard">Dashboard</Link>
                     </li>
                     <li className='menuElement'>
+                        <People 
+                            color={'#00000'}
+                            title={'Legion'}
+                            height="25px"
+                            width="25px"
+                        />
                         <Link to="/legion">Legion</Link>
                     </li>
                     <li className='menuElement'>
+                        <Checkbox
+                            color={'#00000'}
+                            title={'Checklists'}
+                            height="25px"
+                            width="25px"
+                        />
                         <Link to="/checklists">Checklists</Link>
                     </li>
                     {/* Add other menu items */}
